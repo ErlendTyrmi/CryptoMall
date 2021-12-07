5ceca06ce364a693b41d535a7444cd9c2ec7344e
erlendtyrmi <s132962@dtu.dk>
2021-12-07 01:52:57 +0100

    buy button added

--- app/src/main/java/com/erlend/cryptomall/view/ui/composables/trade/Buy.kt
+++ app/src/main/java/com/erlend/cryptomall/view/ui/composables/trade/Buy.kt
@@ -5,6 +5,7 @@
 package com.erlend.cryptomall.view.ui.composables.trade
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -12,8 +13,11 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.erlend.cryptomall.view.ui.composables.asset.AccountTopBar
+import com.erlend.cryptomall.view.viewModels.AccountViewModel
 import com.erlend.cryptomall.view.viewModels.TradeViewModel
 
 // Enter an amount to buy as float, bounded by free dollars in portfolio
@@ -27,30 +31,34 @@ fun Buy(navController: NavHostController, tradeViewModel: TradeViewModel, symbol
     tradeViewModel.pullAssetRemote(symbol)
     tradeViewModel.observeAssetLocal(symbol)
 
-    var text by remember { mutableStateOf("0") }
+    var text by remember { mutableStateOf("") }
 
     Column() {
         TradeTopBar(asset = asset)
         Box(modifier = Modifier.fillMaxSize()){
-            Column(modifier = Modifier.align(Alignment.Center).padding(16.dp, 0.dp)){
+            Column(modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp, 0.dp)){
 
                 TextField(
-                    modifier = Modifier.fillMaxWidth().padding(0.dp, 16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp, 16.dp),
                     value = text,
                     onValueChange = { text = it },
-                    label = { Text("Enter amount") }
+                    label = { Text("Enter amount")},
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                 )
 
                 Button(
-                    modifier = Modifier.fillMaxWidth().padding(0.dp, 16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp, 16.dp),
                     onClick = { navController.navigate("buy/$symbol") }
                 ) {
                     Text(text = "Buy ${asset?.name}", Modifier.padding(8.dp))
                 }
             }
-
         }
-
     }
-
 }
